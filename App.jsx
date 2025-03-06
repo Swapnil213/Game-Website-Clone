d3d89fa7820f7091f7d629325e13006f366d32d7
Swapnil Thawale <92202649+Swapnil213@users.noreply.github.com>
2025-03-06 23:25:51 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import Hero from './components/hero'
-
+import React from "react";
+import Hero from "./components/hero";
+import About from "./components/about";
 
 const App = () => {
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden'>
-      <Hero/>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+      <About />
     </main>
   );
-}
+};
 
-export default App
+export default App;
